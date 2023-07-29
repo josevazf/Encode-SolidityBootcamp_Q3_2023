aa0d25ef39ee706a70871211394e5d56bf09fa05
josevazf <jose.guilherme.rvf@gmail.com>
2023-07-29 10:02:06 +0100

    up

--- Lesson1/HelloWorld.sol
+++ Lesson1/HelloWorld.sol
@@ -3,10 +3,10 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract HelloWorld {
-
     constructor () {}
     
+    /// @notice 
     function helloWorld() public pure returns (string memory) {
-        return "Hello World";
+        return "Hello world!";
     }
 }
