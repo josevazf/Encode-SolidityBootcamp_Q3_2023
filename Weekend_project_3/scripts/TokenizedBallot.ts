d94dcb0ad6d39bdfaa8a030c20239e4d10d8220c
jrocha-v <jrocha-v@student.42porto.com>
2023-08-12 18:52:48 +0100

    up

---
+++ Weekend_project_3/scripts/TokenizedBallot.ts
@@ -0,0 +1,14 @@
+ //   Solidity Bootcamp Q3 2023   \\
+//   Weekend Project 3 - GROUP 6   \\
+
+require('dotenv').config();
+import { ethers } from "ethers";
+
+async function main() {
+	
+}
+
+main().catch((error) => {
+	console.error(error);
+	process.exitCode = 1;
+});
