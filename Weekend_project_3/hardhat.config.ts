5fb2cfe83a0e2690e9f053709b739819e0ace45f
jrocha-v <jrocha-v@student.42porto.com>
2023-08-12 18:51:20 +0100

    included verify config

--- Weekend_project_3/hardhat.config.ts
+++ Weekend_project_3/hardhat.config.ts
@@ -1,9 +1,26 @@
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
+require('dotenv').config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
-  paths: { tests: "tests" },
+	defaultNetwork: "sepolia",
+	networks: {
+	  hardhat: {
+	  },
+	  sepolia: {
+		url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.PROVIDER_KEY}`,
+	  }
+	},
+	etherscan: {
+		apiKey: process.env.ETHERSCAN_API_KEY
+	},
+	solidity: {
+		version: "0.8.19"
+	},
+	paths: { 
+		tests: "tests" 
+	},
 };
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
