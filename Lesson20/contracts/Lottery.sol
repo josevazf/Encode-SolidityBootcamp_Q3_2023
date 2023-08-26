91467f92a828c27b841d50fe2645e88ad31915a1
jrocha-v <jrocha-v@student.42porto.com>
2023-08-26 19:22:46 +0100

    up

--- Lesson20/contracts/Lottery.sol
+++ Lesson20/contracts/Lottery.sol
@@ -15,8 +15,18 @@ contract Lottery is Ownable {
 	/// @notice Timestamp of the lottery next closing date and time
   uint256 public betsClosingTime;
 
-	constructor () {
-		paymentToken = new LotteryToken("name", "SYM");
+
+	/// @notice Constructor function
+	/// @param tokenName Name of the token used for payment
+	/// @param tokenSymbol Symbol of the token used for payment
+	constructor (
+		string memory tokenName, 
+		string memory tokenSymbol, 
+		uint256 _purchaseRatio, 
+		uint256 _betPrice, 
+		uint256 _betFee
+	) {
+		paymentToken = new LotteryToken(tokenName, tokenSymbol);
 	}
 
 	/// @notice Passes when the lottery is at closed state
@@ -34,9 +44,15 @@ contract Lottery is Ownable {
 			_;
 	}
 
-	function openBets(uint256 closingTime) external whenBetsClosed onlyOwner{
+	/// @notice Opens the lottery for receiving bets and sets the closing time
+	function openBets(uint256 closingTime) external whenBetsClosed onlyOwner {
 		require (closingTime > block.timestamp, "Closing time must be in the future");
 		betsClosingTime = closingTime;
 		betsOpen = true;
 	}
+
+
+
+
+
 }
